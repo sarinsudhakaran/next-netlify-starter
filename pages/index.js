a037e2ee4325e23d7481aa519c9be7b3ad66054c
Sarin <sarin.sudha93@gmail.com>
2023-07-31 21:02:37 +0100

    adding style

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,85 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Portfolio - John Doe</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="John Doe's Portfolio" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Welcome to my portfolio website. I'm passionate about coding and design!
         </p>
+        
+        {/* Add your portfolio content here */}
+        <section className="project">
+          <h2>Project 1</h2>
+          <p>Description of Project 1...</p>
+        </section>
+        
+        <section className="project">
+          <h2>Project 2</h2>
+          <p>Description of Project 2...</p>
+        </section>
+
+        {/* Add more portfolio content as needed */}
       </main>
 
       <Footer />
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 2rem;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          color: #fff;
+        }
+
+        .description {
+          font-size: 1.2rem;
+          margin-top: 1rem;
+        }
+
+        .project {
+          margin-top: 2rem;
+          text-align: center;
+        }
+
+        h2 {
+          font-size: 1.5rem;
+        }
+
+        p {
+          font-size: 1.2rem;
+        }
+        
+        /* Black and white theme */
+        body {
+          background-color: #000;
+        }
+
+        main {
+          background-color: #000;
+        }
+
+        h2,
+        p {
+          color: #fff;
+        }
+
+        /* Add more custom styling here as needed */
+      `}</style>
     </div>
   )
 }
